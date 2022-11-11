5bb4408c8f760ae471daefba186c4c576718a87a
sojebsikder <sojebsikder@gmail.com>
2022-11-11 23:09:01 +0600

    main

--- app/controllers/seller/sellerAuth/sellerAuth.controller.ts
+++ app/controllers/seller/sellerAuth/sellerAuth.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { env } from "../../../../system/src";
 import { Controller, Get } from "../../../../system/src/core/decorator";
 import { SellerAuthService } from "./sellerAuth.service";
 
@@ -8,7 +9,12 @@ export class SellerAuthController {
   @Get("login")
   async login(req: Request, res: Response) {
     const token = req.query.token;
-    const login = await SellerAuthService.getInstance().login();
-    res.send(`token: ${token}`);
+    // after sucessfull login set cookie and localstorage
+    res.cookie(env("COOKIE_NAME"), token, {
+      maxAge: env("JWT_EXPIRY"),
+      httpOnly: true,
+      signed: true,
+    });
+    res.redirect("/seller/dashboard");
   }
 }
